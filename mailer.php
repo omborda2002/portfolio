d06db5e78a8c8a286886e8687344c5011af994a4
Om Borda <omborda@Oms-MacBook-Air.local>
2025-03-22 14:05:17 +0100

    first commit

---
+++ mailer.php
@@ -0,0 +1,43 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $mail_to = "omborda2002@gmail.com"; // Updated to your email
+        
+        # Sender Data
+        $name    = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["full-name"])));
+        $email   = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+        $phone   = $_POST["phone-number"];
+        $subject = $_POST["subject"];
+        $budget  = $_POST["budget"];
+        $file    = $_POST["file"];
+        $message = trim($_POST["message"]);
+        
+        if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($message)) {
+            http_response_code(400);
+            echo "Please complete the form and try again.";
+            exit;
+        }
+        
+        # Mail Content
+        $content = "Name: $name\n";
+        $content .= "Subject: $subject\n\n";
+        $content .= "Email: $email\n\n";
+        $content .= "Phone: $phone\n\n";
+        $content .= "Budget: $budget\n\n";
+        $content .= "Message:\n$message\n";
+        $content .= "File: <a href='$file' target='_blank'>$file</a>";
+
+        $headers = "From: $name <$email>";
+
+        $success = mail($mail_to, $name, $content, $headers);
+        if ($success) {
+            http_response_code(200);
+            echo "Thank You! Your message has been sent.";
+        } else {
+            http_response_code(500);
+            echo "Oops! Something went wrong, we couldn't send your message.";
+        }
+    } else {
+        http_response_code(403);
+        echo "There was a problem with your submission, please try again.";
+    }
+?>
